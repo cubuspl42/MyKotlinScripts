d8e87fecd8d5e9e091da092ffebf83aa837f1b03
Jakub Trzebiatowski <cubuspl42@gmail.com>
2023-09-11 17:37:04 +0200

    Upgrade Kotlin plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("jvm") version "1.9.0"
     application
 }
 
@@ -12,8 +12,8 @@ repositories {
     mavenCentral()
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(17)
 }
 
 application {
